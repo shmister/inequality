0944f695df8b97148f0eb5559113d86dc3d3d56c
animathias <mathias.pronin@icloud.com>
2018-12-16 22:16:58 +0200

    grid and shocks are ready

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
+import numpy as np
+import pandas as pd
+import time as time
+
 from params import *
 from utils import *
 
-
-
+from tests import *
 
 def main():
     print("Start Main")
@@ -12,6 +15,15 @@ def main():
 
     # generate aggregate grid
     km = generate_grid(k_min, km_max, ngridkm)
+
+
+    emp_shocks, agg_shocks = generate_shocks(trans_mat= prob, N= 100, T= 110000, Tskip= t_skip)
+    # emp_shocks, agg_shocks = gen_shocks(prob= prob, N= 100, T= 110000)
+    print(emp_shocks.shape, agg_shocks.shape)
+
+    test1(agg_shocks, emp_shocks, T)
+    print(prob)
+
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,14 @@
 import numpy as np
+import quantecon as qe
+import time as tm
+
+from params import ur_b
+
+def p_agg(p_agg_ind):
+    p00 = p_agg_ind[0,0] + p_agg_ind[0,1]
+    p10 = p_agg_ind[2,0] + p_agg_ind[2,1]
+    return np.array([[p00, 1 - p00], [p10, 1 - p10]])
+
 
 def generate_grid(k_min, k_max, n_points, tau=0):
     if tau!=0:
@@ -7,3 +17,28 @@ def generate_grid(k_min, k_max, n_points, tau=0):
         return k_min + (k_max-k_min)*y
     else:
         return np.linspace(k_min, k_max, n_points)
+
+
+def generate_shocks(trans_mat, N, T, Tskip=0):
+    np.random.seed(int(round(tm.time())))
+    # np.random.seed(0)
+    T += Tskip
+
+    agg_trans_mat = p_agg(trans_mat)
+    emp_trans_mat = trans_mat/np.kron(agg_trans_mat, np.ones((2, 2)))
+
+    mc = qe.MarkovChain(agg_trans_mat)
+    agg_shocks = mc.simulate(ts_length=T, init=0)
+    emp_shocks = np.zeros((T, N))
+
+    draw0 = np.random.uniform(size=N)
+    emp_shocks[0, :] = draw0>ur_b
+
+    # generate idiosyncratic shocks for all agents starting in second period
+    draws = np.random.uniform(size=(T-1, N))
+    for t in range(1, T):
+        curr_emp_trans_mat = emp_trans_mat[2*agg_shocks[t-1]: 2*agg_shocks[t-1]+2, 2*agg_shocks[t]: 2*agg_shocks[t]+2]
+        curr_emp_trans_probs =  np.where(emp_shocks[t-1, :]==0.0, curr_emp_trans_mat[0,0], curr_emp_trans_mat[1,1])
+        emp_shocks[t, :] = np.where(curr_emp_trans_probs>draws[t-1, :], emp_shocks[t-1, :], 1 - emp_shocks[t-1, :])
+
+    return emp_shocks[Tskip:, ], agg_shocks[Tskip:]
